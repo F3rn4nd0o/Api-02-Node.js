743c918de342745b37190812749e720362cb40ca
F3rn4nd0o <fernandovasconcelosmelo@outlook.com>
2023-05-25 01:29:00 -0300

    Primeiro Commit

---
+++ middlewares/check-session-id-exist.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.checkSessionIdExist = void 0;
+async function checkSessionIdExist(request, reply) {
+    const sessionId = request.cookies.sessionId;
+    if (!sessionId) {
+        return reply.status(401).send({
+            error: 'Unauthozied.',
+        });
+    }
+}
+exports.checkSessionIdExist = checkSessionIdExist;
